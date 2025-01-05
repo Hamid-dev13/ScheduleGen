659307b066eca74662a2c3620425ddf7f10f2cdb
Hamid-dev13 <hamid.bennacef@epitech.eu>
2025-01-05 23:37:17 +0100

    mise en place du projet

---
+++ schedule-gen/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl text-center font-bold">Générateur d'Emploi du Temps</h1>
+      <button className="mt-4 p-2 bg-[#FF5722] text-white rounded">Commencer</button>
+    </div>
+  );
+}
+
+export default App;
